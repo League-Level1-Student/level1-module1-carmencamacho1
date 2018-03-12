ff7dc5f765da2002b3c24b1aeae46a2206dea6e4
carmencamacho1 <carmen.camacho@jointheleague.org>
2018-03-12 14:29:37 -0700

    Add files via upload

---
+++ RunnerSmurf.java
@@ -0,0 +1,17 @@
+
+public class RunnerSmurf {
+public static void main(String[] args) {
+	Smurf Handy = new Smurf("Handy");
+	Handy.getName();
+	Handy.eat();
+	Smurf Papa = new Smurf("Papa");
+	Papa.getName();
+	Papa.getHatColor();
+	Papa.isGirlOrBoy();
+	Smurf gal = new Smurf("Smurfette");
+	gal.getName();
+	gal.getHatColor();
+	gal.isGirlOrBoy();
+	
+}
+}
